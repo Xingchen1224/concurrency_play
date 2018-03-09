6d4d87c97e398df0f549ab33a1dcbb2c64910202
Xingchen Wang <Xingchen@Xingchens-MacBook-Pro.local>
2018-03-09 09:56:31 +0800

    public class member func.

--- main.cpp
+++ main.cpp
@@ -4,18 +4,38 @@
 
 void f1(std::string && what, int&& x = 0)
 {
-    std::cout << x << " bottle of " << what << std::endl;
+    std::cout << x << " bottles of " << what << std::endl;
 }
+
+void f2(std::string what, int x = 0)
+{
+    std::cout << x << " glasses of " << what << std::endl;
+}
+
+class F3
+{
+public:
+    void f3(size_t data)
+    {
+        ++data;
+        this->m_data = data;
+        std::cout << "data: " << data << std::endl;
+    }
+private:
+    size_t m_data{0};
+};
 
 int main()
 {
-    std::thread t1(f1,"beers",2);
-    
-    std::this_thread::sleep_for(std::chrono::seconds(1));
-    std::cout << "main 1s later!"<<std::endl;
+    std::thread t1(f1,"beer",2); // global func void
     t1.join();
-   
-    std::cout << "main thread quit!"<<std::endl;
+    
+    std::thread t2(f2,"milk",3); // global func with params
+    t2.join();
+    
+    F3 f3_obj;
+    std::thread t3(&F3::f3,&f3_obj, 666);
+    t3.join();
     
     return 0;
 }
